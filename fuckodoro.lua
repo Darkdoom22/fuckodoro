331fbe56a58f0fbcd3e026b3ce795bb4d7ad8116
Darkdoom22 <43715687+Darkdoom22@users.noreply.github.com>
2021-03-11 12:24:11 -0800

    Add files via upload

---
+++ fuckodoro.lua
@@ -0,0 +1,53 @@
+_addon.name = "fuckodoro"
+_addon.version = "1.0"
+
+local packets = require('packets')
+
+
+windower.register_event('incoming chunk', function(id, data)
+
+
+
+    if id == 0x021 then
+
+        windower.add_to_chat(160, 'trade requested')
+        
+        local tradeRequest = packets.parse('incoming', data)
+
+        local trade = packets.new('outgoing', 0x033)
+
+        packets.inject(trade);
+    
+    end
+
+    if id == 0x023 then
+
+        local tradeItem = packets.parse('incoming', data)
+
+        tradeCount = tradeItem["Trade Count"]
+
+    end
+
+    if id == 0x022 then
+
+        local tradeState = packets.parse('incoming', data)
+
+        local tradeOtherParty = windower.ffxi.get_mob_by_id(tradeState["Player"])
+
+        if tradeState["Type"] == 2 then --Trade accepted by other party
+           
+            local tradeFinish = packets.new('outgoing', 0x033)
+
+            tradeFinish["Type"] = 2
+            tradeFinish["Trade Count"] = tradeCount
+
+            packets.inject(tradeFinish)
+
+        end
+
+    end
+
+end)
+
+
+
